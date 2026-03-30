b1e67ea0338b6b0626aceedc73beafc56e155212
q3mrs <123552016+q3mrs@users.noreply.github.com>
2026-03-30 22:58:36 +0300

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,16 @@
 /// <reference types="astro/client" />
 /// <reference types="@titaniumnetwork-dev/ultraviolet/client" />
 ///  <reference types="@mercuryworkshop/scramjet" />
+interface Window {
+  __uv$config: {
+    prefix: string;
+    encodeUrl: (url: string) => string;
+    decodeUrl: (url: string) => string;
+    handler: string;
+    bundle: string;
+    config: string;
+    sw: string;
+  };
+}
+
+declare const __uv$config: any;
